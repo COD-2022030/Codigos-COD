33778d207a8ba49d60c68f4cbaf862d187f5de8c
COD-2022030 <128651500+COD-2022030@users.noreply.github.com>
2023-06-08 07:32:14 -0600

    Create Intensidad_de_un_led_con_Pot_y_PWM.ino

---
+++ Practicas 3er Bimestre/Intensidad_de_un_led_con_Pot_y_PWM/Intensidad_de_un_led_con_Pot_y_PWM.ino
@@ -0,0 +1,23 @@
+/*
+ * Fundacion Kinal
+ * Centro educativo tecnico laboral Kinal
+ * Perito en electonica
+ * Quinto perito
+ * EB5AM 
+ * Taller de electronica digital y reparacion de computadoras
+ * Nombre: Cristian Estuardo Orellana Dieguez
+*/
+
+#define LED 3 
+#define POT A0
+ 
+void setup() {
+ pinMode(LED,OUTPUT);
+ pinMode(POT,INPUT);
+}
+ 
+void loop() {
+  int intensidad; 
+  intensidad = map(analogRead(POT), 0, 1023, 0, 255);
+  analogWrite(LED,intensidad); 
+}
